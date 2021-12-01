530e964e41304ad8d716b74376a3a0cdeb674bdc
Jan Kruska <jan.kruska@isf.rwth-aachen.de>
2021-12-01 18:49:42 +0100

    Init for dfg plotting

--- apps/plots/views.py
+++ apps/plots/views.py
@@ -14,23 +14,18 @@ from pm4pymdl.algo.mvp.utils import (
     succint_mdl_to_exploded_mdl,
     exploded_mdl_to_succint_mdl,
 )
+from pm4pymdl.algo.mvp.gen_framework3 import discovery
+from pm4pymdl.visualization.mvp.gen_framework3 import visualizer as visualizer
+
 
 import modules.plots as plots
 from apps.index import models
 import modules.utils as utils
 
 
-class PlotsView(View):
+class HistogramView(View):
     def get(self, request, column=None):
-        log_type = request.GET.get("type")
-        if log_type == "event_log":
-            event_log = models.EventLog.objects.get(id=request.GET.get("id"))
-        elif log_type == "event_cube":
-            event_log = models.EventLog.objects.get(
-                id=request.GET.get("id")
-            )  # Todo plotting for cube
-        else:
-            raise Http404("no such log type supported")
+        event_log = models.EventLog.objects.get(id=request.GET.get("id"))
         df, obj_df = ocel_importer.apply(event_log.file.path)
         numerical, categorical, objects = utils.get_column_types(df)
         obj_numerical, obj_categorical, _ = utils.get_column_types(obj_df)
@@ -60,3 +55,46 @@ class PlotsView(View):
             link_text="",
         )
         return render(request, "plots/raw.html", context={"object": plot_div})
+    
+class DFGView(View):
+    def get(self, request):
+        event_log = models.EventLog.objects.get(id=request.GET.get("id"))
+        
+        df, obj_df = ocel_importer.apply(event_log.file.path)
+
+        
+        model = discovery.apply(df, parameters={"epsilon": 0, "noise_threshold": 0})
+        gviz = visualizer.apply(model, parameters={"min_act_freq": 5, "min_edge_freq": 5})
+
+        return render(request, "plots/raw.html")
+
+# def dfg_to_g6(dfg):
+#     unique_nodes = []
+#     # print(dfg)
+#     for i in dfg:
+#         unique_nodes.extend(i)
+#     unique_nodes = list(set(unique_nodes))
+
+#     unique_nodes_dict = {}
+
+#     for index, node in enumerate(unique_nodes):
+#         unique_nodes_dict[node] = "node_" + str(index)
+
+#     nodes = [{'id': unique_nodes_dict[i], 'name': i, 'isUnique':False, 'conf': [
+#         {
+#             'label': 'Name',
+#             'value': i
+#         }
+#     ]} for i in unique_nodes_dict]
+#     freqList = [int(dfg[i]) for i in dfg]
+#     maxVal = max(freqList) if len(freqList) != 0 else 0
+#     minVal = min(freqList) if len(freqList) != 0 else 0
+
+#     edges = [{'source': unique_nodes_dict[i[0]], 'target': unique_nodes_dict[i[1]], 'label': round(dfg[i], 2),
+#               "style": {"lineWidth": ((int(dfg[i]) - minVal) / (maxVal - minVal) * (20 - 2) + 2), "endArrow": True}} for
+#              i in
+#              dfg]
+#     data = {
+#         "nodes": nodes,
+#         "edges": edges,
+#     }
